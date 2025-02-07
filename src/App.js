1da5d31c2c559e5e9311c9f7d4937fced2c5e00c
Suhaib K S <suhaibcoorg5@gmail.com>
2025-02-08 00:17:53 +0530

    Update App.js
    set to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
